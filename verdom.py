788da9fac6969e3b4627426d32ae307435a9e8e9
Leonardo Barros <nickolback@gmail.com>
2018-03-08 12:59:21 -0300

    Comando ping

--- verdom.py
+++ verdom.py
@@ -1,5 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import subprocess
 import argparse
+
+parser = argparse.ArgumentParser(prog="verdom")
+parser.add_argument("-p", action="store_true", help="Ping um ip")
+
+args = parser.parse_args()
+if args.p:
+    ip = input("Informe o ip/host: ")
+    print(ip)
+    subprocess.run(['ping', '-c4', ip])
