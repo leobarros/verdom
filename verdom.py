21fe706e9c3b1375bd48685def025d421146670a
Leonardo Barros <nickolback@gmail.com>
2018-03-08 20:10:24 -0300

    Colocando em funcoes

--- verdom.py
+++ verdom.py
@@ -8,7 +8,16 @@ parser = argparse.ArgumentParser(prog="verdom")
 parser.add_argument("-p", action="store_true", help="Ping um ip")
 
 args = parser.parse_args()
-if args.p:
-    ip = input("Informe o ip/host: ")
-    print(ip)
-    subprocess.run(['ping', '-c4', ip])
+#if args.p:
+#    ip = input("Informe o ip/host: ")
+#    print(ip)
+#    subprocess.run(['ping', '-c4', ip])
+
+def app():
+    if args.p:
+        ip = input("Informe o ip/host: ")
+        search = subprocess.call(['ping', '-c4', ip])
+        print(search)
+
+
+app()
