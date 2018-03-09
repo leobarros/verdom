6f657001d3d7feb53bc05775e4c980fd204d47bd
Leonardo Barros <nickolback@gmail.com>
2018-03-08 21:04:59 -0300

    Comando host e remocao de variaveis

--- verdom.py
+++ verdom.py
@@ -6,18 +6,35 @@ import argparse
 
 parser = argparse.ArgumentParser(prog="verdom")
 parser.add_argument("-p", action="store_true", help="Ping um ip")
+parser.add_argument('-d', action="store_true", help="Checar dados de um domínio")
 
 args = parser.parse_args()
-#if args.p:
-#    ip = input("Informe o ip/host: ")
-#    print(ip)
-#    subprocess.run(['ping', '-c4', ip])
 
 def app():
     if args.p:
         ip = input("Informe o ip/host: ")
-        search = subprocess.call(['ping', '-c4', ip])
-        print(search)
+        subprocess.call(['ping', '-c4', ip])
+
+    if args.d:
+        dominio = input("Informe um dominio: ")
+        print("\n***MX***")
+        subprocess.call(['host', '-t', 'mx', dominio])
+
+        print("\n***Name Server***")
+        subprocess.call(['host', '-t', 'NS', dominio])
+
+        print("\n***IPV4***")
+        subprocess.call(['host', '-t' 'A', dominio])
+
+        print("\n***IPV6***")
+        subprocess.call(['host', '-t', 'AAAA', dominio])
+
+        print("\n***TXT***")
+        subprocess.call(['host', '-t', 'TXT', dominio])
+
+        print("\n***SPF***")
+        subprocess.call(['host', '-t', 'SPF', dominio])
+
 
 
 app()
